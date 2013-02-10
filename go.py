341dc2888f2695c96217c089f6482e0b4bd8488b
Meghan Clark <mclark@oakwoodcontrols.com>
2013-02-10 17:39:42 -0500

    Fixed global counter in go

--- go.py
+++ go.py
@@ -3,7 +3,7 @@ from time import time
 from stop import *
 import elementtree.ElementTree as ET
 
-global_uid_counter = 0
+uid_counter = 0
 
 def get_all_umich_stops():
    ##################
@@ -43,8 +43,9 @@ def get_all_umich_stops():
                if (stop.tag == 'longitude'):
                   lng = stop.text
                if (stop.tag == 'id1'):
-                  uid = global_uid_counter
-                  global_uid_counter += 1
+                  global uid_counter
+                  uid = uid_counter
+                  uid_counter += 1
                   cur_stop = Stop(uid, names, ['UMICH'], [route_name], lat, lng)
                   stops.append(cur_stop)
    return stops
